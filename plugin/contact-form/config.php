5c5826feaec9aa583b1ee28993d2ef03c8c0528d
Liam Docherty <liam_docherty@outlook.com>
2019-03-17 18:07:38 +0000

    commit

---
+++ plugin/contact-form/config.php
@@ -0,0 +1,18 @@
+<?php
+		define('CONTACT_FORM_NAME','https://webmail.hostinger.co.uk');
+		define('CONTACT_FORM_EMAIL','nfo@zeplur.com');
+
+		define('CONTACT_FORM_SMTP_HOST','smtp.hostinger.co.uk');
+		define('CONTACT_FORM_SMTP_USER','info@zeplur.com');
+		define('CONTACT_FORM_SMTP_PORT','995');
+		define('CONTACT_FORM_SMTP_SECURE','587');
+		define('CONTACT_FORM_SMTP_PASSWORD','liam1999');
+
+		define('CONTACT_FORM_SUBJECT','Auto Spa - Car Wash Auto Detail Template: Contact');
+
+		define('CONTACT_FORM_MSG_INVALID_DATA_NAME','Please enter your name.');
+		define('CONTACT_FORM_MSG_INVALID_DATA_EMAIL','Please enter valid e-mail.');
+		define('CONTACT_FORM_MSG_INVALID_DATA_MESSAGE','Please enter your message.');
+
+		define('CONTACT_FORM_MSG_SEND_OK','Thank you for contacting us.');
+		define('CONTACT_FORM_MSG_SEND_ERROR','Sorry, we can\'t send this message.');
